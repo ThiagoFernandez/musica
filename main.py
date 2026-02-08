e0ea6e3e3704e39d2a27529a569acff68a3e6388
ThiagoFernandez <thiagofprocacci@gmail.com>
2026-02-08 16:23:39 -0300

    added more things/ FInish the main menu

--- main.py
+++ main.py
@@ -8,25 +8,58 @@ import os
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 import pygame
 import random
+import shutil
+import time
+import mutagen
+from mutagen.mp3 import MP3
 
 def download_music(playlist):
-    url = input(
-        "Copy the link from youtube\n"
-        "To paste use 'ctrl + shift + v'\n>>>: "
-    ).strip()
+    while True:
+        try:
+            option = int(input(f"1 -> song\n2 -> playlist\n3 -> exit\nChoose an option: "))
+            if option == 3:
+                return None
+            else:
+                if option < 1 or option > 3:
+                    print("The option must be between 1-3 | Try again")
+                else:
+                    if option == 1:
+                        url = input(
+                            "Copy the link from youtube\n"
+                            "To paste use 'ctrl + shift + v'\n>>>: "
+                        ).strip()
 
-    output_path = os.path.join(LIBRARY_DIR, playlist, "%(title)s.%(ext)s")
+                        output_path = os.path.join(LIBRARY_DIR, playlist, "%(title)s.%(ext)s")
 
-    subprocess.run([
-        sys.executable,
-        "-m", "yt_dlp",
-        "--no-playlist", # para el file settings lo mas probable
-        "-x",
-        "--audio-format", "mp3",
-        "--audio-quality", "0",
-        "-o", output_path,
-        url
-    ])
+                        subprocess.run([
+                            sys.executable,
+                            "-m", "yt_dlp",
+                            "--no-playlist", # para el file settings lo mas probable
+                            "-x",
+                            "--audio-format", "mp3",
+                            "--audio-quality", "0",
+                            "-o", output_path,
+                            url
+                        ])
+                    else:
+                        url = input(
+                            "Copy the link from youtube\n"
+                            "To paste use 'ctrl + shift + v'\n>>>: "
+                        ).strip()
+
+                        output_path = os.path.join(LIBRARY_DIR, playlist, "%(title)s.%(ext)s")
+
+                        subprocess.run([
+                            sys.executable,
+                            "-m", "yt_dlp",
+                            "-x",
+                            "--audio-format", "mp3",
+                            "--audio-quality", "0",
+                            "-o", output_path,
+                            url
+                        ])
+        except ValueError:
+            print("The option must be a number | Try again")
 
 def create_playlist():
     new_playlist = input("Write the name of your new playlist: ").strip()
@@ -39,117 +72,51 @@ def create_playlist():
         print(f"The playlist - {new_playlist} was created")
     return None
 
-def song_controller_input_light():
-    step = 0.1
-    playing = True
-
-    print("\n[P]ause | [R]esume | [S]top | [U]p vol | [D]own vol | [E]xit")
-
-    while playing:
-        option = input(">>> ").strip().upper()
-
-        match option:
-            case "P":
-                pygame.mixer.music.pause()
-
-            case "R":
-                pygame.mixer.music.unpause()
-
-            case "S":
-                pygame.mixer.music.stop()
-                playing = False 
-                return None
-
-            case "U":
-                vol = pygame.mixer.music.get_volume()
-                pygame.mixer.music.set_volume(min(1.0, vol + step))
-                print(f"Volume: {pygame.mixer.music.get_volume():.1f}")
-
-            case "D":
-                vol = pygame.mixer.music.get_volume()
-                pygame.mixer.music.set_volume(max(0.0, vol - step))
-                print(f"Volume: {pygame.mixer.music.get_volume():.1f}")
-
-            case "E":
-                return  None
-
-            case _:
-                print("Invalid command")    
 
 def song_controller_input():
+    print("\n[P]ause | [R]esume | [U] Vol+ | [D] Vol- | [N]ext | [S]top")
+    option = input(">>> ").strip().upper()
+
     step = 0.1
-    playing = True
 
-    print("\n[P]ause | [R]esume | [S]top | [U]p vol | [D]own vol | [N]ext | [E]xit")
+    match option:
+        case "P":
+            pygame.mixer.music.pause()
+        case "R":
+            pygame.mixer.music.unpause()
+        case "U":
+            vol = pygame.mixer.music.get_volume()
+            pygame.mixer.music.set_volume(min(1.0, vol + step))
+        case "D":
+            vol = pygame.mixer.music.get_volume()
+            pygame.mixer.music.set_volume(max(0.0, vol - step))
+        case "N":
+            pygame.mixer.music.stop()
+            return 1
+        case "S":
+            pygame.mixer.music.stop()
+            return -1
 
-    while playing:
-        option = input(">>> ").strip().upper()
-
-        match option:
-            case "P":
-                pygame.mixer.music.pause()
-
-            case "R":
-                pygame.mixer.music.unpause()
-
-            case "S":
-                pygame.mixer.music.stop()
-                playing = False 
-                return -1
-
-            case "U":
-                vol = pygame.mixer.music.get_volume()
-                pygame.mixer.music.set_volume(min(1.0, vol + step))
-                print(f"Volume: {pygame.mixer.music.get_volume():.1f}")
-
-            case "D":
-                vol = pygame.mixer.music.get_volume()
-                pygame.mixer.music.set_volume(max(0.0, vol - step))
-                print(f"Volume: {pygame.mixer.music.get_volume():.1f}")
-
-            case "E":
-                return -1
-            
-            case "N":
-                return 1
-
-            case _:
-                print("Invalid command")
+    return 0
 
 
 
 def play_song(song_path):
+    print(f"\n▶ Reproduciendo: {os.path.basename(song_path)}")
+
     pygame.mixer.music.load(song_path)
     pygame.mixer.music.play()
 
-    state = song_controller_input()
-    if state == -1:
-        return -1
-    else:
-        return None
+    while pygame.mixer.music.get_busy():
+        state = song_controller_input()
 
-# modificar para que sea global y no solo una playlist
-# def pick_song(playlist):
-#     all_songs = os.listdir(f"/home/zanto/Desktop/musica/library/{playlist}")
-#     if len(all_songs) == 0:
-#         print(f"Songs found = 0\nBack to the menu")
-#         return None
-#     else:
-#         while True:
-#             for idx, song in enumerate(all_songs):
-#                 print(f"{i+1}. {song}")
-#             print(f"{len(all_songs)+1}. Exit")
-#             try:
-#                 option = int(input("Choose an option: "))
-#                 if option == len(all_songs)+1:
-#                     print("Back to the menu")
-#                     return None
-#                 else:
-#                     if option < 1 or option > len(all_songs):
-#                         song_path = os.path.join("library", playlist, all_songs[option-1])
-#                         play_song(song_path)
-#             except ValueError:
-#                 print("The option must be a number | Try again")
+        if state == -1:
+            return -1   
+        elif state == 1:
+            return 1   
+
+    return 0  
+
 
 def play_playlist(playlist):
     playlist_path = os.path.join(LIBRARY_DIR, playlist)
@@ -178,23 +145,28 @@ def play_playlist(playlist):
 
             if option == 1:
                 songs = all_songs.copy()
-                while songs:
-                    song = random.choice(songs)
-                    songs.remove(song)
+                random.shuffle(songs)
+
+                for song in songs:
                     song_path = os.path.join(playlist_path, song)
                     state = play_song(song_path)
-                    if state== -1:
+                    if state == -1:
                         return None
-
+                    elif state == 1:
+                        print("Skipping song...")
+                        continue
             else:
                 for song in all_songs:
                     song_path = os.path.join(playlist_path, song)
                     state = play_song(song_path)
                     if state == -1:
                         return None
-
+                    elif state == 1:
+                        print("Skipping song...")
+                        continue
         except ValueError:
             print("The option must be a number | Try again")
+
 
 def pick_playlist():
     all_playlists = os.listdir(LIBRARY_DIR)
@@ -220,6 +192,126 @@ def pick_playlist():
             except ValueError:
                 print("The option must be a number | Try again")
 
+def delete_playlist():
+    playlist = pick_playlist()
+    if playlist != None:
+        path = os.path.join(LIBRARY_DIR, playlist)
+        try:
+            shutil.rmtree(path)
+            print(f"Directory '{path}' and all its contents deleted successfully.")
+        except FileNotFoundError:
+            print(f"Directory '{path}' not found.")
+        except Exception as e:
+            print(f"Error deleting directory: {e}")
+    return None
+
+def delete_song():
+    playlist = pick_playlist()
+    if playlist != None:
+        playlist_path = os.path.join(LIBRARY_DIR, playlist)
+        all_songs = os.listdir(playlist_path)
+        for i, song in enumerate(all_songs):
+            print(f"{i+1}. {song}")
+        print(f"{len(all_songs)+1}. Exit")
+        while True:
+            try:
+                option = int(input("Choose an option: "))
+                if option == len(all_songs)+1:
+                    return None
+                else:
+                    song_path = os.path.join(playlist_path, all_songs[option-1])
+                    os.remove(song_path)
+                    return None
+            except ValueError:
+                print("The option must be a number | Try again")
+
+def delete_what():
+    while True:
+        print(f"1 -> Song\n2 -> Playlist\n3 ->Exit")
+        try:
+            option = int(input("Choose an option: "))
+            if option == 3:
+                return None
+            else:
+                if option < 1 or option > 3:
+                    print("The option must be between 1-3 | Try again")
+                else:
+                    if option == 1:
+                        delete_song()
+                        return None
+                    else:
+                        delete_playlist()
+                        return None
+        except ValueError:
+            print("The option must be a number | Try again")
+
+def change_directory():
+    playlist = pick_playlist()
+    if playlist != None:
+        playlist_path = os.path.join(LIBRARY_DIR, playlist)
+        all_songs = os.listdir(playlist_path)
+        for i, song in enumerate(all_songs):
+            print(f"{i+1}. {song}")
+        print(f"{len(all_songs)+1}. Exit")
+        while True:
+            try:
+                option = int(input("Choose an option: "))
+                if option == len(all_songs)+1:
+                    return None
+                else:
+                    song_path = os.path.join(playlist_path, all_songs[option-1])
+                    song = all_songs[option-1]
+                    new_playlist= pick_playlist()
+                    new_dir = os.path.join(LIBRARY_DIR, new_playlist, song)
+                    if new_dir != None:
+                        try:
+                            shutil.copyfile(song_path, new_dir)
+                            print(f"File '{song_path}' copied to '{new_dir}' successfully.")
+                            os.remove(song_path)
+                        except shutil.SameFileError:
+                            print("Source and destination represent the same file.")
+                        except PermissionError:
+                            print("Permission denied.")
+                        except FileNotFoundError:
+                            print("Source file not found.")
+                        except Exception as e:
+                            print(f"An error occurred: {e}")
+                    return None
+            except ValueError:
+                print("The option must be a number | Try again")
+        
+
+def main_menu():
+    while True:
+        print(f"{'Welcome to the jambot':-^60}\n1. Pick a playlist\n2. Create a playlist\n3. Delete a playlist/song\n4. Download a song/playlist\n5. Change song's playlist\n6. Exit")
+        while True:
+            try:
+                option = int(input("Choose an option: "))
+                if option < 1 or option >7:
+                    print("The option must be between 1-6 | Try again")
+                else:
+                    break
+            except ValueError:
+                print("The option must be a number | Try again")
+        match option:
+            case 1:
+                playlist = pick_playlist()
+                if playlist != None:
+                    play_playlist(playlist)
+            case 2:
+                create_playlist()
+            case 3:
+                delete_what()
+            case 4:
+                print("--Which playlist")
+                playlist = pick_playlist()
+                if playlist != None:
+                    download_music(playlist)
+            case 5:
+                change_directory()
+                pass
+            case 6:
+                break
 
 #m.p
 BASE_DIR = "/home/zanto/Desktop/musica"
@@ -230,35 +322,5 @@ pygame.mixer.init()
 pygame.mixer.music.set_volume(0.5) # tambien puede llegar a estar en un file de settings
 SONG_END = pygame.USEREVENT + 1
 pygame.mixer.music.set_endevent(SONG_END)
+main_menu()
 
-while True:
-    print(f"{'Welcome to the jambot':-^60}\n1. Pick a playlist\n2. Create a playlist\n3. Delete a playlist\n4. Download a song\n5. Change song's playlist\n6. Music controller\n7. Exit")
-    while True:
-        try:
-            option = int(input("Choose an option: "))
-            if option < 1 or option >7:
-                print("The option must be between 1-7 | Try again")
-            else:
-                break
-        except ValueError:
-            print("The option must be a number | Try again")
-    match option:
-        case 1:
-            playlist = pick_playlist()
-            if playlist != None:
-                play_playlist(playlist)
-        case 2:
-            create_playlist()
-        case 3:
-            pass
-        case 4:
-            print("--Which playlist")
-            playlist = pick_playlist()
-            if playlist != None:
-                download_music(playlist)
-        case 5:
-            pass
-        case 6:
-            song_controller_input_light() # temporal el light, la idea es poder skippear dsd aca pero tengo que buscar como
-        case 7:
-            break
